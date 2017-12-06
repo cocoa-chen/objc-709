a38c29ae134d351a286b621336b7cc0a8665fa5c
chenaibin <chenaibin@didichuxing.com>
2017-12-06 12:00:37 +0800

    Init project

---
+++ debug-objc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  debug-objc
+//
+//  Created by 陈爱彬 on 2017/12/6. Maintain by 陈爱彬
+//  Description 
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
